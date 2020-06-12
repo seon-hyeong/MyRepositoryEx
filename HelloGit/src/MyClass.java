23876ab75f693988f81d6c877b07ab9fc01fa6c2
seonhyeong <752312@naver.com>
2020-06-12 16:50:45 +0900

    Fist Commit in dev branch !

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -4,7 +4,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Git !!!!!!! ");
-		new MyHello().sayHello();
+		MyHello myHello = new MyHello();
+		myHello.sayHello();
 
 	}
 
--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -2,7 +2,9 @@
 public class MyHello {
 
 	public void sayHello() {
-		System.out.println("Hello!");
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
 
 	}
 
